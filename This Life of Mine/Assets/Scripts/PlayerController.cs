7301881ffca7d820ac812b3259867e6948dfa7be
Martin Devenney <themadrebellion@gmail.com>
2019-05-19 17:01:19 +0100

    Added simple character movement
    PlayerMovement - handles the movement of the rigidbody.
    PlayerController - orders the movement of the player.

---
+++ This Life of Mine/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    PlayerMovement playerMove = new PlayerMovement();
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerMove.MovePlayer(rb);
+    }
+}
---
+++ This Life of Mine/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement
+{
+    Vector3 direction;
+
+    public float speed = 10f;
+
+    public void MovePlayer(Rigidbody rb)
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+}

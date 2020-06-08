badc7e7472396589028d10da31db7bd6ee002fc1
Martin Devenney <themadrebellion@gmail.com>
2020-06-08 13:37:42 +0100

    Small changes
    Nothing note worthy. Just deleted some objects that are not needed or have been in the project for a very long time that have not been used.

---
+++ This Life of Mine/Assets/Scripts/Player/MoveCreator_Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class MoveCreator_Test : MonoBehaviour
+{
+    public List<int> comboListTest = new List<int>();
+    public List<Motion> comboList = new List<Motion>();
+
+    int comboIndex = 0;
+    int movesetAdded = 0;
+
+    public Animator anim;
+    public AnimatorController animatorController;
+    Motion move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            comboListTest.Add(movesetAdded);
+            movesetAdded++;
+            animatorController.AddMotion(comboList[comboIndex]);
+            comboIndex++;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (comboListTest.Count > comboIndex)
+            {
+                Debug.Log(comboListTest[comboIndex]);
+                comboIndex++;
+            }
+            else
+                return;
+
+            //animatorController.AddMotion(comboList[comboIndex]);
+        }
+    }
+}
